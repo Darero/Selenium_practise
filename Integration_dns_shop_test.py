99a336a2dad7acee994092aebe8d6eea1bbd4dd4
Darero <max.mironov2011@yandex.ru>
2022-08-05 16:15:32 +0300

    added simple cart test

--- Integration_dns_shop_test.py
+++ Integration_dns_shop_test.py
@@ -1,7 +1,9 @@
-import pytest
+import time, pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 @pytest.fixture
@@ -43,7 +45,7 @@ class TestSearchBar:
         self.initiate_searching(browser, self.good_article)
         assert browser.current_url == self.expected_link
 
-    def test_should_lead_to_defined_category(self, browser):
+    def test_should_lead_to_defined_category(self, browser):  # APPLY TO SINGLE WORD CATEGORIES ONLY
         # print('Performing category search test')
         self.initiate_searching(browser, self.category)
         page_h1 = browser.find_element(By.CSS_SELECTOR, 'h1.title').text
@@ -58,3 +60,42 @@ class TestSearchBar:
         assert 'категориях' not in page_h1.lower() or 'часы' in page_h1.lower()
 
 
+class TestCart:
+    url = 'https://www.dns-shop.ru/'
+    goods_articles = {'notebook': 4874678, 'accessory': 1323034,
+                      'bonuses': 4890553, 'online_payment': 4839637}
+    searchbar_selector = '#header-search [placeholder=\'Поиск по сайту\']'
+
+    def initiate_searching(self, test_browser, value):
+        # print('Beginning search...')
+        searchbar = test_browser.find_element(By.CSS_SELECTOR, self.searchbar_selector)
+        searchbar.send_keys(value)
+        searchbar.send_keys(Keys.ENTER)
+        # print('Search complete...')
+        return test_browser
+
+    def test_all_goods_should_be_in_cart(self, browser):
+        browser.get(self.url)
+        for category, article in self.goods_articles.items():
+            self.initiate_searching(browser, article)
+
+            WebDriverWait(browser, 5).until(
+                EC.all_of(
+                    EC.visibility_of_element_located((By.CSS_SELECTOR, '.product-card-top__buy .buy-btn')),
+                    EC.element_to_be_clickable((By.CSS_SELECTOR, '.product-card-top__buy .buy-btn'))
+                )
+            )
+            # WebDriverWait(browser, 10).until(
+            #     EC.visibility_of_element_located((By.CSS_SELECTOR, '.product-card-top__buy .buy-btn')))
+            #
+            # WebDriverWait(browser, 10).until(
+            #     EC.element_to_be_clickable((By.CSS_SELECTOR, '.product-card-top__buy .buy-btn')))
+            browser.find_element(By.CSS_SELECTOR, '.product-card-top__buy .buy-btn').click()
+            time.sleep(5)
+        browser.find_element(By.CLASS_NAME, 'cart-link').click()
+        articles = []
+        for i in range(len(self.goods_articles)):
+            article = browser.find_element(By.CSS_SELECTOR,
+                                 f'div.cart-items__product:nth-child({2 + i}) .cart-items__product-code').text
+            articles.append(article)
+        assert all(map(lambda x: int(x) in self.goods_articles.values(), articles))
